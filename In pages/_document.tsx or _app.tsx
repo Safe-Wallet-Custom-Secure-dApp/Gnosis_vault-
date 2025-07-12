35830b53afe2d77e216f7b9f077a6d71df00852e
thegood.eth <172343141+thegoodeth12@users.noreply.github.com>
2025-07-12 21:12:27 +0100

    Create _document.tsx or _app.tsx

---
+++ In pages/_document.tsx or _app.tsx
@@ -0,0 +1,5 @@
+<link rel="manifest" href="/manifest.json" />
+<link rel="apple-touch-icon" href="/icon.png" />
+<meta name="theme-color" content="#0a0a0a" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black" />
